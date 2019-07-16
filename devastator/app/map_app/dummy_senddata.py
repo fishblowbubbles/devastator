f118f0b97ddf4f288995d2e51f365a59af716e9b
Cheryl <cherylgohqp@users.noreply.github.com>
2019-07-16 21:56:37 +0800

    uploaded current map gui interface (excludes button function because button function is not functional)
    features include:
    1) listens for a client (robot) initiating connection with app to send data about distance and angle from robot to app
    2) moves the robot according to current position calculated based on how far robot is from markers, on the map
    3) able to plot markers for "threat", "suspect", "person" on the map at positions seen by robot
    4) displays "scanning.." when robot is doing a scan for object detection

---
+++ devastator/app/map_app/dummy_senddata.py
@@ -0,0 +1,25 @@
+from helpers import *
+
+HOST = "localhost"
+PORT = 8998
+
+#Assuming data sent is in the form of distance,angle
+
+data = {"action":("moving") ,"marker":(2),"distanceToMarker":(4),"angleToMarker":(60)}
+# data = {"action":("scanning") ,"objectsDetected":("THREAT"),"distanceToObject":(200),"angletoObject":(20)}
+# data = {"action":("scanning") ,"objectsDetected":("PERSON"),"distanceToObject":(2),"angletoObject":(20)}
+#Note:
+# objectsDetected can be "SUSPECT", "THREAT" or "PERSON"
+# action can be either "scanning" or "moving"
+# distance and angle are all given by the intel realsense camera
+
+connect_and_send(data,HOST,PORT)
+
+
+# server =  socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# server.bind(("localhost",8998))
+# server.listen()
+# while True:
+#     connection, address = server.accept()
+#     connection.sendall(pickle.dumps(data))
+#     connection.close()
