4db5583fec9901dcb9e6cb75028243df17e1f1eb
chenwenshu <wenshu_chen@mymail.sutd.edu.sg>
2019-07-19 16:53:22 +0800

    add scripts to measure the focal length

--- devastator/vision/Aruco_Tracker/aruco_tracker.py
+++ devastator/vision/Aruco_Tracker/aruco_tracker.py
@@ -1,18 +1,30 @@
 import numpy as np
 import sys
-sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+
+if ('/opt/ros/kinetic/lib/python2.7/dist-packages') in sys.path:
+    sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+
 import cv2
 import cv2.aruco as aruco
 import glob
-import socket
+import numpy as np
 import pickle
+import socket
 import timeit
+
+marker_side = 15.5  # cm
+device = "cam"
+
 
 def get_frame(input_stream, HOST=None, PORT=None):
     if input_stream == "cam":
         input_stream = 0 
         cap = cv2.VideoCapture(input_stream)
         ret, frame = cap.read()
+
+        if not cap.isOpened():
+            cap.open(0)
+
     elif input_stream == "server":
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
             client.connect((HOST, PORT))
@@ -34,6 +46,21 @@ def recv_object(client):
         packets.append(packet)
     object = pickle.loads(b"".join(packets))
     return object
+
+
+def get_side_length(corners):
+    sides_list = []
+
+    for i in range(len(corners[0]) - 1, -1, -1):
+        corner1 = corners[0][i - 1]
+        corner2 = corners[0][i]
+
+        dist = np.linalg.norm(corner1 - corner2)
+        sides_list.append(dist)
+
+    return sum(sides_list) / len(sides_list)
+
+
 start = timeit.default_timer()
 HOST = "127.0.0.1"
 PORT = 4445
@@ -81,10 +108,13 @@ ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.sh
 num_frames = 0
 
 while (True):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        client.connect((HOST, PORT))
-        frame = recv_object(client)
-        frame, depth = cv2.cvtColor(frame[:, :, :3].astype(np.uint8), cv2.COLOR_RGB2BGR), frame[:, :, 3].astype(np.uint8)
+    if device == "cam":
+        frame = get_frame('cam')
+    else:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+            client.connect((HOST, PORT))
+            frame = recv_object(client)
+            frame, depth = cv2.cvtColor(frame[:, :, :3].astype(np.uint8), cv2.COLOR_RGB2BGR), frame[:, :, 3].astype(np.uint8)
     # operations on the frame
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -119,18 +149,19 @@ while (True):
         # horizontal angle that the marker is away from the center line of sight of the robot 
         # value from (-43.5 deg to + 43.5 deg) where 43.5 is left/right?
 
-        
         for i in range(len(corners)):
             center_x = int(corners[i].mean(axis=1)[0][0])
             center_y = int(corners[i].mean(axis=1)[0][1])
             marker_h_angle = round((center_x - 640) * (87/1280),2)
-            depth_point = round(depth[center_y, center_x]/1000,2)
+            #depth_point = round(depth[center_y, center_x]/1000,2)
+            depth_point = 0
             marker_details["angleToMarker"] = marker_h_angle
-            marker_details["distanceToMarker"] = depth_point
+            #marker_details["distanceToMarker"] = depth_point
             marker_details["id"] = ids[i]
             markers.append(marker_details)
             cv2.putText(frame, str(depth_point) + "m, " + str(marker_h_angle) + " deg",
                         (int(corners[i][0][0][0]), int(corners[i][0][0][1]) - 7), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0,255,0), 1)
+            print(get_side_length(corners[i]))
 
         # draw a square around the markers            
         aruco.drawDetectedMarkers(frame, corners)
