cd4e1c7277e7ebe84f0203891f8f360972b67897
Cheryl <cherylgohqp@yahoo.com.sg>
2019-08-02 16:42:42 +0800

    updates report logs files with temp face recog

--- devastator/app/report_log/receive_from_bot.py
+++ devastator/app/report_log/receive_from_bot.py
@@ -1,9 +1,11 @@
 import json
 import socket, pickle
-#ROBOT TO SEND report logs TO PORT 8886
+from devastator.app.Facerecognition.Call_Face_Rec import load_known, guess_who, frametest
+
+#ROBOT TO SEND report logs TO PORT 8888
 HOST = "192.168.1.136"
 # HOST = "192.168.1.185"
-PORT = 8886
+PORT = 8888
 data = {}
 
 def recv_obj(s):
@@ -27,7 +29,27 @@ def update_data():
                 data = recv_obj(connection)
                 print(data)
                 curr_data = json.load(open('reports.json'))
-                print(curr_data)
+                # print(curr_data)
+
+                enc, names = load_known()
+                # print(guess_who(None, enc, names))
+                for key in data:
+                    data[key]['Persons_Detected'] = "Person Detected: <p/>"
+                    for i in  data[key]['Persons_Detected']:
+                        person = guess_who(i, enc, names)
+                        add_person_to_list = str(person) + " <p/>"
+                        data[key]['Persons_Detected'] += add_person_to_list
+
+                    #for testins --------------------------------------------------
+                    # person = guess_who(None, enc, names)
+                    # data[key]['Persons_Detected'] = "Person Detected: <p/>" + str(person)
+                    #--------------------------------------------------------------
+                    for i in data[key]['Guns_Detected']:
+                        ### gun classification algo
+                        data[key]['Guns_Detected'] = "Guns Detected: <p/>" + "SAR21"
+
+                print(data)
+
                 curr_data['data'].update(data)
                 with open('reports.json', 'w') as outfile:
                     json.dump(curr_data, outfile,
